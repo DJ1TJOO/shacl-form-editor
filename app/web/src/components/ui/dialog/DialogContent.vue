df0d1fa33be2f4f6c7e4a6bdc34cac921cc6d475
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-11-25 10:30:26 +0100

    chore: added dialog based on shadcn-vue

---
+++ app/web/src/components/ui/dialog/DialogContent.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { cn } from '@/lib/cn'
+import { reactiveOmit } from '@vueuse/core'
+import { X } from 'lucide-vue-next'
+import type { DialogContentEmits, DialogContentProps } from 'reka-ui'
+import { DialogClose, DialogContent, DialogPortal, useForwardPropsEmits } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import DialogOverlay from './DialogOverlay.vue'
+
+const props = defineProps<DialogContentProps & { class?: HTMLAttributes['class'] }>()
+const emits = defineEmits<DialogContentEmits>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
+
+<template>
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogContent
+      data-slot="dialog-content"
+      v-bind="forwarded"
+      :class="
+        cn(
+          'top-[50%] left-[50%] z-50 fixed gap-4 grid bg-background shadow-lg p-6 rounded-lg w-full max-w-[calc(100%-2rem)] sm:max-w-lg translate-x-[-50%] translate-y-[-50%] data-[state=closed]:animate-out data-[state=open]:animate-in duration-200 data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
+          props.class,
+        )
+      "
+      @pointer-down-outside.prevent
+      @interact-outside.prevent
+    >
+      <slot />
+
+      <DialogClose
+        class="top-4 right-4 absolute hover:bg-background-highlighted p-0.5 rounded-md transition-colors"
+      >
+        <X />
+        <span class="sr-only">Close</span>
+      </DialogClose>
+    </DialogContent>
+  </DialogPortal>
+</template>
---
+++ app/web/src/components/ui/dialog/DialogScrollContent.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { cn } from '@/lib/cn'
+import { reactiveOmit } from '@vueuse/core'
+import { X } from 'lucide-vue-next'
+import type { DialogContentEmits, DialogContentProps } from 'reka-ui'
+import { DialogClose, DialogContent, DialogPortal, useForwardPropsEmits } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import DialogOverlay from './DialogOverlay.vue'
+
+const props = defineProps<DialogContentProps & { class?: HTMLAttributes['class'] }>()
+const emits = defineEmits<DialogContentEmits>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
+
+<template>
+  <DialogPortal>
+    <DialogOverlay class="place-items-center grid overflow-y-auto">
+      <DialogContent
+        :class="
+          cn(
+            'z-50 relative gap-4 grid bg-background shadow-lg my-8 p-6 sm:rounded-lg w-full md:w-full max-w-lg duration-200',
+            props.class,
+          )
+        "
+        v-bind="forwarded"
+        @pointer-down-outside="
+          (event) => {
+            const originalEvent = event.detail.originalEvent
+            const target = originalEvent.target as HTMLElement
+            if (
+              originalEvent.offsetX > target.clientWidth ||
+              originalEvent.offsetY > target.clientHeight
+            ) {
+              event.preventDefault()
+            }
+          }
+        "
+        @interact-outside.prevent
+      >
+        <slot />
+
+        <DialogClose
+          class="top-4 right-4 absolute hover:bg-background-highlighted p-0.5 rounded-md transition-colors"
+        >
+          <X class="w-4 h-4" />
+          <span class="sr-only">Close</span>
+        </DialogClose>
+      </DialogContent>
+    </DialogOverlay>
+  </DialogPortal>
+</template>

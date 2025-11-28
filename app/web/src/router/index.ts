df7fea2a397a5ad98e30a809f62d0dd769695723
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-11-28 16:59:11 +0100

    chore: renmae to kebab-case

--- app/web/src/router/index.ts
+++ app/web/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EditorView from '../views/EditorView.vue'
+import EditorView from '../views/editor-view.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),

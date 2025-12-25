ab4e1cc6e910717b1a146c5d55433fa0d403654e
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2025-12-25 17:09:36 +0100

    fix: new scrollbar sticky

--- app/web/src/components/editor-bar/bar.vue
+++ app/web/src/components/editor-bar/bar.vue
@@ -23,44 +23,46 @@ const namespaces = Namespaces.useActiveNamespacesDefinitions()
 </script>
 
 <template>
-  <ScrollArea class="top-0 z-10 sticky bg-background">
-    <nav data-slot="editor-bar" class="flex flex-nowrap h-10">
-      <FileItem />
-      <ShapeItem
-        v-for="shape in shapes"
-        :key="shape.value"
-        :to="`/file/${fileId}/${encodeURIComponent(shape.value)}`"
-        @click="
-          () => {
-            router.replace(`/file/${fileId}/${encodeURIComponent(shape.value)}`)
-            if (activeTab !== 'turtle') return
+  <div class="top-0 z-10 sticky">
+    <ScrollArea class="bg-background">
+      <nav data-slot="editor-bar" class="flex flex-nowrap h-10">
+        <FileItem />
+        <ShapeItem
+          v-for="shape in shapes"
+          :key="shape.value"
+          :to="`/file/${fileId}/${encodeURIComponent(shape.value)}`"
+          @click="
+            () => {
+              router.replace(`/file/${fileId}/${encodeURIComponent(shape.value)}`)
+              if (activeTab !== 'turtle') return
 
-            const prefixedShape = Prefixes.absoluteToPrefixed(
-              namespaces,
-              file.implicitBase,
-              shape.value,
-            )
-            scrollToShape(prefixedShape)
-          }
-        "
-        :label="shape.name"
-        :type="shape.type"
-        @removeShape="
-          () => {
-            removeShape(shape.value)
-            if (!route.path.includes(encodeURIComponent(shape.value))) return
-
-            const nextShape = shapes[0]
-            if (nextShape) {
-              router.push(`/file/${fileId}/${encodeURIComponent(nextShape.value)}`)
-            } else {
-              router.push(`/file/${fileId}/`)
+              const prefixedShape = Prefixes.absoluteToPrefixed(
+                namespaces,
+                file.implicitBase,
+                shape.value,
+              )
+              scrollToShape(prefixedShape)
             }
-          }
-        "
-      />
-      <NewItem />
-    </nav>
-    <ScrollBar orientation="horizontal" :offset="-1" :size="1.5" />
-  </ScrollArea>
+          "
+          :label="shape.name"
+          :type="shape.type"
+          @removeShape="
+            () => {
+              removeShape(shape.value)
+              if (!route.path.includes(encodeURIComponent(shape.value))) return
+
+              const nextShape = shapes[0]
+              if (nextShape) {
+                router.push(`/file/${fileId}/${encodeURIComponent(nextShape.value)}`)
+              } else {
+                router.push(`/file/${fileId}/`)
+              }
+            }
+          "
+        />
+        <NewItem />
+      </nav>
+      <ScrollBar orientation="horizontal" :offset="-1" :size="1.5" />
+    </ScrollArea>
+  </div>
 </template>

7beb9158dfd8c470aba02090eab27546d3ddf472
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2026-01-12 17:19:53 +0100

    chore: remove clarity

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,15 +1,10 @@
 import './assets/main.css'
 
-import Clarity from '@microsoft/clarity'
 import VueDnDKitPlugin from '@vue-dnd-kit/core'
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 import { createApp } from 'vue'
 import App from './app.vue'
 import router from './router'
-
-const projectId = import.meta.env.VITE_CLARITY_PROJECT_ID
-Clarity.init(projectId)
-Clarity.consentV2({ ad_Storage: 'denied', analytics_Storage: 'denied' })
 
 self.MonacoEnvironment = {
   getWorker() {

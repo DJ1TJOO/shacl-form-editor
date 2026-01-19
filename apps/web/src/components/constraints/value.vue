1a96b499c5764cca9eec04dceac68b94da40dc83
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2026-01-19 14:29:42 +0100

    fix: value constraints to adapt input field based on nodekind and datatype

--- apps/web/src/components/constraints/value.vue
+++ apps/web/src/components/constraints/value.vue
@@ -2,19 +2,90 @@
 import { Constraint, type ConstraintProps } from '@/components/constraints'
 import { RemoveButton } from '@/components/form-ui/buttons'
 import { FieldList, FieldOptional } from '@/components/form-ui/field'
+import { PrefixInput } from '@/components/form-ui/prefix'
 import { Shacl, Xsd } from '@/components/rdf'
 import { Field, FieldLabel } from '@/components/ui/field'
 import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
-import { useLiteral, useLiteralList } from '@/composables/use-shacl'
+import { useLiteral, useLiteralList, useNamed, useNamedList } from '@/composables/use-shacl'
 import { InfoIcon } from 'lucide-vue-next'
-import { Literal } from 'rdflib'
+import { Literal, NamedNode } from 'rdflib'
+import { computed } from 'vue'
 
 const { subject } = defineProps<ConstraintProps>()
 
-// @TODO: Input type for value should be based on the datatype, class and/or nodekind. It could be a named node or a literal.
-const { value: hasValue } = useLiteral<string>({ subject, predicate: Shacl.SHACL('hasValue') })
-const { items: inValues } = useLiteralList({ subject, predicate: Shacl.SHACL('in') })
+const { value: nodeKind } = useNamed({
+  subject,
+  predicate: Shacl.SHACL('nodeKind'),
+  readonly: true,
+})
+const { value: datatype, node: datatypeNode } = useNamed({
+  subject,
+  predicate: Shacl.SHACL('datatype'),
+  readonly: true,
+})
+const isDatatypeDecimal = computed(() => (datatype.value ? Xsd.isDecimal(datatype.value) : false))
+const isDatatypeInteger = computed(() => (datatype.value ? Xsd.isInteger(datatype.value) : false))
+const isDatatypeDate = computed(() => (datatype.value ? Xsd.isDate(datatype.value) : false))
+const isDatatypeDateTime = computed(() => (datatype.value ? Xsd.isDateTime(datatype.value) : false))
+
+const { value: hasValueLiteral } = useLiteral<string>({
+  subject,
+  predicate: Shacl.SHACL('hasValue'),
+})
+const { value: hasValueNamed } = useNamed({
+  subject,
+  predicate: Shacl.SHACL('hasValue'),
+})
+const hasValue = computed({
+  get() {
+    if (nodeKind.value === Shacl.SHACL('IRI').value) {
+      return hasValueNamed.value
+    }
+    return hasValueLiteral.value
+  },
+  set(value: string | undefined) {
+    if (nodeKind.value === Shacl.SHACL('IRI').value) {
+      if (typeof value !== 'string' && typeof value !== 'undefined') return
+      hasValueNamed.value = value
+      return
+    }
+    hasValueLiteral.value = value
+  },
+})
+
+const { items: inValuesLiteral } = useLiteralList({ subject, predicate: Shacl.SHACL('in') })
+const { items: inValuesNamed } = useNamedList({
+  subject,
+  predicate: Shacl.SHACL('in'),
+})
+const inValues = computed({
+  get() {
+    if (nodeKind.value === Shacl.SHACL('IRI').value) {
+      return inValuesNamed.map((item) => ({ value: item.value }))
+    }
+    return inValuesLiteral.map((item) => ({ value: item.value }))
+  },
+  set(value: { value: string }[]) {
+    if (nodeKind.value === Shacl.SHACL('IRI').value) {
+      inValuesNamed.splice(
+        0,
+        inValuesNamed.length,
+        ...value.map((item) => ({ value: item.value, node: new NamedNode(item.value) })),
+      )
+      return
+    }
+    inValuesLiteral.splice(
+      0,
+      inValuesLiteral.length,
+      ...value.map((item) => ({
+        value: item.value,
+        node: new Literal(item.value),
+        datatype: datatypeNode.value ?? Xsd.string,
+      })),
+    )
+  },
+})
 </script>
 
 <template>
@@ -28,9 +99,30 @@ const { items: inValues } = useLiteralList({ subject, predicate: Shacl.SHACL('in
         </Tooltip>
       </FieldLabel>
 
-      <FieldOptional v-model="hasValue" :create="() => ''" v-slot="{ remove }">
+      <FieldOptional
+        v-if="nodeKind === Shacl.SHACL('IRI').value"
+        v-model="hasValueNamed"
+        :create="() => ':'"
+        v-slot="{ remove }"
+      >
+        <PrefixInput v-model="hasValueNamed">
+          <RemoveButton @click="remove" />
+        </PrefixInput>
+      </FieldOptional>
+      <FieldOptional v-else v-model="hasValue" :create="() => ''" v-slot="{ remove }">
         <InputGroup>
-          <InputGroupInput v-model="hasValue" />
+          <InputGroupInput
+            v-model="hasValue"
+            :type="
+              isDatatypeDecimal || isDatatypeInteger
+                ? 'number'
+                : isDatatypeDate
+                  ? isDatatypeDateTime
+                    ? 'datetime-local'
+                    : 'date'
+                  : 'text'
+            "
+          />
           <InputGroupAddon align="inline-end">
             <RemoveButton @click="remove" />
           </InputGroupAddon>
@@ -48,12 +140,48 @@ const { items: inValues } = useLiteralList({ subject, predicate: Shacl.SHACL('in
       </FieldLabel>
 
       <FieldList
+        v-if="nodeKind === Shacl.SHACL('IRI').value"
         v-slot="{ entry, remove }"
-        v-model="inValues"
-        :create="() => ({ value: '', node: new Literal(''), datatype: Xsd.string })"
+        v-model="inValuesNamed"
+        :create="() => ({ value: '', node: new NamedNode('') })"
       >
         <InputGroup>
-          <InputGroupInput v-model="entry.value" />
+          <InputGroupInput
+            v-model="entry.value"
+            :type="
+              isDatatypeDecimal || isDatatypeInteger
+                ? 'number'
+                : isDatatypeDate
+                  ? isDatatypeDateTime
+                    ? 'datetime-local'
+                    : 'date'
+                  : 'text'
+            "
+          />
+          <InputGroupAddon align="inline-end">
+            <RemoveButton @click="remove" />
+          </InputGroupAddon>
+        </InputGroup>
+      </FieldList>
+      <FieldList
+        v-else
+        v-slot="{ entry, remove }"
+        v-model="inValues"
+        :create="() => ({ value: '' })"
+      >
+        <InputGroup>
+          <InputGroupInput
+            v-model="entry.value"
+            :type="
+              isDatatypeDecimal || isDatatypeInteger
+                ? 'number'
+                : isDatatypeDate
+                  ? isDatatypeDateTime
+                    ? 'datetime-local'
+                    : 'date'
+                  : 'text'
+            "
+          />
           <InputGroupAddon align="inline-end">
             <RemoveButton @click="remove" />
           </InputGroupAddon>
